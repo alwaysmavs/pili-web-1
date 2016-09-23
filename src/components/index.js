05408ed28269885c2114c2ea9b9b4987c28bab5b
wushuang <beingswu@gmail.com>
2016-09-23 22:25:05 +0800

    seed

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>12123123123</div>
+            </div>
+
+        )
+    }
+}
+
+
+
+export default App;
