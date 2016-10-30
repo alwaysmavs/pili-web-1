6d4f8df70a7bc750085fe4833988d657b00d0e06
wushuang <beingswu@gmail.com>
2016-10-30 19:21:35 +0800

    price done

--- src/components/price/price.js
+++ src/components/price/price.js
@@ -5,8 +5,195 @@ class Price extends Component {
     render() {
         return (
             <div className="price-outbox">
-                <div className="price-part1-bg"></div>
-                <div className="price-part1"></div>
+                <div className="price-part1-bg">
+                    <div className="price-part1-title">
+                        价格方案
+                    </div>
+                </div>
+                <div className="price-part1">
+                    <div  className="price-part1-list">
+                        <div className="price-line-box">
+                            <div className="price-line1">
+                                免费
+                            </div>
+                            <div className="price-line2">
+                                选择此方案
+                            </div>
+                        </div>
+
+                        <div className="price-line3">
+                            SDK 基本功能
+                        </div>
+                        <div className="price-line3 text-cut">
+                            高级自定义功能
+                        </div>
+                        <div className="price-line3">
+                            仅 Release 版本推送
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3 text-cut">
+                            线上支持
+                        </div>
+                        <div className="price-line3 text-cut">
+                            驻场支持
+                        </div>
+                        <div className="price-line3">
+                            工单支持
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3">
+                            50 次／月的接口回调
+                        </div>
+                        <div className="price-line3">
+                            50 次／月的接口查询
+                        </div>
+                        <div className="price-line3">
+                            可创建 3 个应用
+                        </div>
+                    </div>
+
+                    <div  className="price-part1-list">
+                        <div className="price-line-box">
+                            <div className="price-line1">
+                                免费
+                            </div>
+                            <div className="price-line2">
+                                选择此方案
+                            </div>
+                        </div>
+
+                        <div className="price-line3">
+                            SDK 基本功能
+                        </div>
+                        <div className="price-line3 text-cut">
+                            高级自定义功能
+                        </div>
+                        <div className="price-line3">
+                            仅 Release 版本推送
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3 text-cut">
+                            线上支持
+                        </div>
+                        <div className="price-line3 text-cut">
+                            驻场支持
+                        </div>
+                        <div className="price-line3">
+                            工单支持
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3">
+                            50 次／月的接口回调
+                        </div>
+                        <div className="price-line3">
+                            50 次／月的接口查询
+                        </div>
+                        <div className="price-line3">
+                            可创建 3 个应用
+                        </div>
+                    </div>
+
+                    <div  className="price-part1-list">
+                        <div className="price-line-box">
+                            <div className="price-line1">
+                                免费
+                            </div>
+                            <div className="price-line2">
+                                选择此方案
+                            </div>
+                        </div>
+
+                        <div className="price-line3">
+                            SDK 基本功能
+                        </div>
+                        <div className="price-line3 text-cut">
+                            高级自定义功能
+                        </div>
+                        <div className="price-line3">
+                            仅 Release 版本推送
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3 text-cut">
+                            线上支持
+                        </div>
+                        <div className="price-line3 text-cut">
+                            驻场支持
+                        </div>
+                        <div className="price-line3">
+                            工单支持
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3">
+                            50 次／月的接口回调
+                        </div>
+                        <div className="price-line3">
+                            50 次／月的接口查询
+                        </div>
+                        <div className="price-line3">
+                            可创建 3 个应用
+                        </div>
+                    </div>
+
+                    <div  className="price-part1-list">
+                        <div className="price-line-box">
+                            <div className="price-line1">
+                                免费
+                            </div>
+                            <div className="price-line2">
+                                选择此方案
+                            </div>
+                        </div>
+
+                        <div className="price-line3">
+                            SDK 基本功能
+                        </div>
+                        <div className="price-line3 text-cut">
+                            高级自定义功能
+                        </div>
+                        <div className="price-line3">
+                            仅 Release 版本推送
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3 text-cut">
+                            线上支持
+                        </div>
+                        <div className="price-line3 text-cut">
+                            驻场支持
+                        </div>
+                        <div className="price-line3">
+                            工单支持
+                        </div>
+                        <div className="cutline"></div>
+                        <div className="price-line3">
+                            50 次／月的接口回调
+                        </div>
+                        <div className="price-line3">
+                            50 次／月的接口查询
+                        </div>
+                        <div className="price-line3">
+                            可创建 3 个应用
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="price-part2">
+                    <div className="price-part2-left">
+                        定制需求可以进一步了解我们的企业服务。
+                    </div>
+                    <div className="price-part2-right">
+                        了解企业服务
+                    </div>
+                </div>
+
+                <div className="price-part3">
+                    <div className="cutline"></div>
+                    <div  className="price-part3-inner">
+                        1 基本功能：<br/>
+                        2 高级功能：高级自选美颜，脸部特效，多滤镜选择等等（这里面的功能大部分基础功能肯定有，但是没有这么牛，比如滤镜效果少）<br/>
+                        3 驻唱支持：专职的技术实施人员，前往贵公司进行服务，每次时效是一天。
+                    </div>
+                </div>
             </div>
         )
     }
