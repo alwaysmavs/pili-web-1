232b1401474000c3aa7b224b7ce901dfbb431414
maverick <beingswu@gmail.com>
2016-11-26 23:36:42 +0800

    test

--- src/constants/docData.js
+++ src/constants/docData.js
@@ -1,14 +1,14 @@
 export const PLAYER_DOC = {
     pageName:"播放器",
-    releaseIosUrl:"http://www.baidu.com",
-    releaseAndroidUrl:"",
-    beginIosUrl:"",
-    beginAndroidUrl:"",
+    releaseIosUrl:"https://alwaysmavs.gitbooks.io/plplayerkit/content/5%20%E5%8E%86%E5%8F%B2%E6%9B%B4%E6%96%B0%E8%AE%B0%E5%BD%95/",
+    releaseAndroidUrl:"https://alwaysmavs.gitbooks.io/pldroidplayer/content/4%20%E5%8E%86%E5%8F%B2%E6%9B%B4%E6%96%B0%E8%AE%B0%E5%BD%95/",
+    beginIosUrl:"https://alwaysmavs.gitbooks.io/plplayerkit/content/2%20%E5%BF%AB%E9%80%9F%E5%BC%80%E5%A7%8B/",
+    beginAndroidUrl:"https://alwaysmavs.gitbooks.io/pldroidplayer/content/2%20%E5%BF%AB%E9%80%9F%E5%BC%80%E5%A7%8B/",
     iosDemoUrl:"",
     androidDemoUrl:"",
     designDocUrl:"",
-    developerIosUrl:"",
-    developerAndroidUrl:"",
+    developerIosUrl:"https://alwaysmavs.gitbooks.io/plplayerkit/content/",
+    developerAndroidUrl:"https://alwaysmavs.gitbooks.io/pldroidplayer/content/",
 };
 
 export const RTC_DOC = {
