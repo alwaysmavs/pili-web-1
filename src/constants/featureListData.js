6bb2e1e918f24ff063d05047d75cdc4936d5d289
maverick <beingswu@gmail.com>
2016-11-27 00:18:54 +0800

    up data

--- src/constants/featureListData.js
+++ src/constants/featureListData.js
@@ -1,23 +1,63 @@
 export const RTC_FEATURELIST = [
     {
         featureId: 1,
-        featureName:"踢人"
+        featureName:"连麦对讲"
     },
     {
         featureId: 2,
-        featureName:"踢人2"
+        featureName:"视频合流"
     },
     {
         featureId: 3,
-        featureName:"踢人2"
+        featureName:"音频混音"
     },
     {
         featureId: 4,
-        featureName:"踢人2"
+        featureName:"支持外部采集视频"
     },
     {
         featureId: 5,
-        featureName:"踢人2"
+        featureName:"支持外部采集音频"
+    },
+    {
+        featureId: 6,
+        featureName:"水印功能"
+    },
+    {
+        featureId: 7,
+        featureName:"摄像头切换"
+    },
+    {
+        featureId: 8,
+        featureName:"自动对焦"
+    },
+    {
+        featureId: 9,
+        featureName:"踢人功能"
+    },
+    {
+        featureId: 10,
+        featureName:"内置和外置美颜"
+    },
+    {
+        featureId: 11,
+        featureName:"内置丰富的消息回调"
+    },
+    {
+        featureId: 12,
+        featureName:"纯音频连麦"
+    },
+    {
+        featureId: 13,
+        featureName:"连麦大小窗口切换"
+    },
+    {
+        featureId: 14,
+        featureName:"左右双屏模式"
+    },
+    {
+        featureId: 15,
+        featureName:"丰富的参数配置"
     }
 ];
 
@@ -47,22 +87,38 @@ export const PLAYER_FEATURELIST = [
 export const PUSH_FEATURELIST = [
     {
         featureId: 1,
-        featureName:"踢人"
+        featureName:"支持纯音频推流"
     },
     {
         featureId: 2,
-        featureName:"踢人2"
+        featureName:"支持后台音频推流"
     },
     {
         featureId: 3,
-        featureName:"踢人2"
+        featureName:"支持自定义滤镜"
     },
     {
         featureId: 4,
-        featureName:"踢人2"
+        featureName:"内置水印功能"
     },
     {
         featureId: 5,
-        featureName:"踢人2"
+        featureName:"内置美颜功能"
+    },
+    {
+        featureId: 6,
+        featureName:"支持背景音乐功能"
+    },
+    {
+        featureId: 7,
+        featureName:"支持耳机反听功能"
+    },
+    {
+        featureId: 8,
+        featureName:"报障功能"
+    },
+    {
+        featureId: 9,
+        featureName:"质量数据上报功能"
     }
 ];
