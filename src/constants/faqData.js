c3d94037f7f0d45b182c9a7ad0ddec50a2f3fb60
maverick <beingswu@gmail.com>
2016-11-30 11:30:03 +0800

    faq-rtc

--- src/constants/faqData.js
+++ src/constants/faqData.js
@@ -3,14 +3,38 @@ export const RTC_FAQ = {
     pageFaq:[
         {
             faqId: 1,
-            faqTitle:"连麦 SDK 需要替换以前的推流 SDK 吗？",
-            faqInner:"是的，连麦 SDK 是在推流 SDK 基础上增加了",
+            faqTitle:"普通观众端是否需要专用的播放器？",
+            faqInner:"不需要，任何支持 RTMP/HLS 协议的媒体播放器均可播放。",
             faqUrl:"http://www.baidu.com"
         },
         {
             faqId: 2,
-            faqTitle:"连麦 SDK 需要替换以前的推流 SDK 吗？",
+            faqTitle:"点播回放是否也能看到连麦画面？",
             faqInner:"是的，连麦 SDK 是在推流 SDK 基础上增加了",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 3,
+            faqTitle:"连麦对讲延时多大 ？",
+            faqInner:"正常网络条件下，连麦对讲延时在 500ms~1s 以内",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 4,
+            faqTitle:"观众端看到的合流画面的延时多大？",
+            faqInner:"观众端看到的合流画面延时，在 2~5s 左右",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 5,
+            faqTitle:"最大支持多少路同时连麦和合流？",
+            faqInner:"理论上只要带宽和性能足够，没有限制的，推荐在 8 路以下。 ",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 6,
+            faqTitle:"连麦 SDK 是否支持硬编?",
+            faqInner:"支持硬编，且可以自由切换",
             faqUrl:"http://www.baidu.com"
         }
     ]
@@ -51,14 +75,32 @@ export const PLAYER_FAQ = {
     pageFaq:[
         {
             faqId: 1,
-            faqTitle:"连麦 SDK 需要替换以前的推流 SDK 吗？",
-            faqInner:"是的，连麦 SDK 是在推流 SDK 基础上增加了",
+            faqTitle:"普通观众端是否需要专用的播放器？",
+            faqInner:"不需要，任何支持 RTMP/HLS 协议的媒体播放器均可播放。",
             faqUrl:"http://www.baidu.com"
         },
         {
             faqId: 2,
-            faqTitle:"连麦 SDK 需要替换以前的推流 SDK 吗？",
+            faqTitle:"点播回放是否也能看到连麦画面？",
             faqInner:"是的，连麦 SDK 是在推流 SDK 基础上增加了",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 3,
+            faqTitle:"连麦对讲延时多大 ？",
+            faqInner:"正常网络条件下，连麦对讲延时在 500ms 左右，网络一般的情况下也能保持 1s 以内",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 4,
+            faqTitle:"观众端看到的合流画面的延时多大？",
+            faqInner:"观众端看到的合流画面延时，在 2~5s 左右",
+            faqUrl:"http://www.baidu.com"
+        },
+        {
+            faqId: 5,
+            faqTitle:"最大支持多少路同时连麦和合流？",
+            faqInner:"理论上只要带宽和性能足够，没有限制的，推荐在 8 路以下。 ",
             faqUrl:"http://www.baidu.com"
         }
     ]
