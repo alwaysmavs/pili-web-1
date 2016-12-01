73e6e85d80757994693791e5ec04ac7c9c9daada
maverick <beingswu@gmail.com>
2016-12-01 15:06:13 +0800

    122

--- src/constants/docData.js
+++ src/constants/docData.js
@@ -13,12 +13,10 @@ export const PLAYER_DOC = {
 export const RTC_DOC = {
     pageName:"连麦",
     serveSdkName:"Server SDK 下载",
-    releaseIosUrl:"http://www.baidu.com",
-    releaseAndroidUrl:"",
-    beginIosUrl:"",
-    beginAndroidUrl:"",
-    developerIosUrl:"",
-    developerAndroidUrl:"",
+    releaseIosUrl:"https://alwaysmavs.gitbooks.io/plrtcios/content/3%20%E5%8E%86%E5%8F%B2%E6%9B%B4%E6%96%B0%E8%AE%B0%E5%BD%95/",
+    releaseAndroidUrl:"https://alwaysmavs.gitbooks.io/plrtcdroid/content/3%20%E5%8E%86%E5%8F%B2%E6%9B%B4%E6%96%B0%E8%AE%B0%E5%BD%95/",
+    developerIosUrl:"https://alwaysmavs.gitbooks.io/plrtcios/content/",
+    developerAndroidUrl:"https://alwaysmavs.gitbooks.io/plrtcdroid/content/",
     iosDownLoadUrl:"http://obyikocz6.bkt.clouddn.com/%E4%B8%83%E7%89%9B%E8%BF%9E%E9%BA%A6SDK-11-16-iOS.zip",
     AndroidDownloadUrl:"http://obyikocz6.bkt.clouddn.com/%E4%B8%83%E7%89%9B%E8%BF%9E%E9%BA%A6SDK-11-16-Android-1.0.1.zip",
     ServerDownloadUrl:"http://obyikocz6.bkt.clouddn.com/%E4%B8%83%E7%89%9B%E8%BF%9E%E9%BA%A6SDK-Server.zip"
