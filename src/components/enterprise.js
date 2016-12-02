81e308a23ff37c71d9ffaa66bcc6a26315513aca
maverick <beingswu@gmail.com>
2016-12-02 10:51:04 +0800

    11

--- src/components/enterprise.js
+++ src/components/enterprise.js
@@ -77,60 +77,61 @@ class Enterprise extends Component {
                 <div className="enterprise-part2">
                     <HomepageBg />
                 </div>
-                <div className="enterprise-part3">
-                    <div className="enterprise-part3-left">
-                        <div className="enterprise-part3-title">客户评价</div>
-                        <div className="enterprise-part3-inner">一心为你，做好解决直播难题的最佳帮手。</div>
-                    </div>
-                    <div className="enterprise-part3-right">
-                        <div className="enterprise-part3-cell">
-                            <div className="enterprise-part3-imgbox">
-                                <img className="enterprise-part3-img"
-                                     src="http://ofji7fgcp.bkt.clouddn.com/xuhuan.png"/>
-                            </div>
-                            <div className="enterprise-part3-textbox">
-                                <div className="enterprise-part3-custom">
-                                    <span className="enterprise-part3-name">徐华</span>
-                                    <span className="enterprise-part3-position">沪江网副总裁</span>
-                                </div>
-                                <div className="enterprise-part3-text">
-                                    七牛为沪江提供了稳定高可用的云端文件大规模存储服务，很好地帮助企业降低了运营成本，提高了响应速度。
-                                </div>
-                            </div>
-                        </div>
 
-                        <div className="enterprise-part3-cell2">
-                            <div className="enterprise-part3-imgbox">
-                                <img className="enterprise-part3-img"
-                                     src="http://ofji7fgcp.bkt.clouddn.com/zhaoyongli.png"/>
-                            </div>
-                            <div className="enterprise-part3-textbox">
-                                <div className="enterprise-part3-custom">
-                                    <span className="enterprise-part3-name">阮永丽</span>
-                                    <span className="enterprise-part3-position">美图秀秀 CTO</span>
-                                </div>
-                                <div className="enterprise-part3-text">
-                                    七牛为沪江提供了稳定高可用的云端文件大规模存储服务，很好地帮助企业降低了运营成本，提高了响应速度。
-                                </div>
-                            </div>
-                        </div>
-                        <div className="enterprise-part3-cell2">
-                            <div className="enterprise-part3-imgbox">
-                                <img className="enterprise-part3-img"
-                                     src="http://ofji7fgcp.bkt.clouddn.com/wangwei.png"/>
-                            </div>
-                            <div className="enterprise-part3-textbox">
-                                <div className="enterprise-part3-custom">
-                                    <span className="enterprise-part3-name">汪威</span>
-                                    <span className="enterprise-part3-position">咸蛋家 CTO</span>
-                                </div>
-                                <div className="enterprise-part3-text">
-                                    七牛为沪江提供了稳定高可用的云端文件大规模存储服务，很好地帮助企业降低了运营成本，提高了响应速度。
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {/*<div className="enterprise-part3">*/}
+                    {/*<div className="enterprise-part3-left">*/}
+                        {/*<div className="enterprise-part3-title">客户评价</div>*/}
+                        {/*<div className="enterprise-part3-inner">一心为你，做好解决直播难题的最佳帮手。</div>*/}
+                    {/*</div>*/}
+                    {/*<div className="enterprise-part3-right">*/}
+                        {/*<div className="enterprise-part3-cell">*/}
+                            {/*<div className="enterprise-part3-imgbox">*/}
+                                {/*<img className="enterprise-part3-img"*/}
+                                     {/*src="http://ofji7fgcp.bkt.clouddn.com/xuhuan.png"/>*/}
+                            {/*</div>*/}
+                            {/*<div className="enterprise-part3-textbox">*/}
+                                {/*<div className="enterprise-part3-custom">*/}
+                                    {/*<span className="enterprise-part3-name">徐华</span>*/}
+                                    {/*<span className="enterprise-part3-position">沪江网副总裁</span>*/}
+                                {/*</div>*/}
+                                {/*<div className="enterprise-part3-text">*/}
+                                    {/*七牛为沪江提供了稳定高可用的云端文件大规模存储服务，很好地帮助企业降低了运营成本，提高了响应速度。*/}
+                                {/*</div>*/}
+                            {/*</div>*/}
+                        {/*</div>*/}
+
+                        {/*<div className="enterprise-part3-cell2">*/}
+                            {/*<div className="enterprise-part3-imgbox">*/}
+                                {/*<img className="enterprise-part3-img"*/}
+                                     {/*src="http://ofji7fgcp.bkt.clouddn.com/zhaoyongli.png"/>*/}
+                            {/*</div>*/}
+                            {/*<div className="enterprise-part3-textbox">*/}
+                                {/*<div className="enterprise-part3-custom">*/}
+                                    {/*<span className="enterprise-part3-name">阮永丽</span>*/}
+                                    {/*<span className="enterprise-part3-position">美图秀秀 CTO</span>*/}
+                                {/*</div>*/}
+                                {/*<div className="enterprise-part3-text">*/}
+                                    {/*七牛为沪江提供了稳定高可用的云端文件大规模存储服务，很好地帮助企业降低了运营成本，提高了响应速度。*/}
+                                {/*</div>*/}
+                            {/*</div>*/}
+                        {/*</div>*/}
+                        {/*<div className="enterprise-part3-cell2">*/}
+                            {/*<div className="enterprise-part3-imgbox">*/}
+                                {/*<img className="enterprise-part3-img"*/}
+                                     {/*src="http://ofji7fgcp.bkt.clouddn.com/wangwei.png"/>*/}
+                            {/*</div>*/}
+                            {/*<div className="enterprise-part3-textbox">*/}
+                                {/*<div className="enterprise-part3-custom">*/}
+                                    {/*<span className="enterprise-part3-name">汪威</span>*/}
+                                    {/*<span className="enterprise-part3-position">咸蛋家 CTO</span>*/}
+                                {/*</div>*/}
+                                {/*<div className="enterprise-part3-text">*/}
+                                    {/*七牛为沪江提供了稳定高可用的云端文件大规模存储服务，很好地帮助企业降低了运营成本，提高了响应速度。*/}
+                                {/*</div>*/}
+                            {/*</div>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
 
             </div>
         )
